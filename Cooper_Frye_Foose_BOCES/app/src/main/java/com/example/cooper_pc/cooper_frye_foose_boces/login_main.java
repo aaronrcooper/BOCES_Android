174f220f6e9a9e83d1cbbfc50e20faf3620ba057
aaronrcooper <mr.aaroncooper@gmail.com>
2017-09-21 14:28:09 -0400

    Android Studio Initial Commit
    Initial commit for the Android Studio initialization files.

---
+++ Cooper_Frye_Foose_BOCES/app/src/main/java/com/example/cooper_pc/cooper_frye_foose_boces/login_main.java
@@ -0,0 +1,13 @@
+package com.example.cooper_pc.cooper_frye_foose_boces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+    }
+}
